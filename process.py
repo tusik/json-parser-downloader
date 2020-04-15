a3dc64e22427e6e687af716f9a0f4ae513266fd9
Image <image@by.cx>
2020-04-15 14:51:50 +0800

    first commit

---
+++ process.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+import psycopg2
+import requests
+import sys,json,os
+from threading import Thread
+
+def download_img(path,url):
+    r = requests.get(url, headers=headers)
+    filename_t = str(url).split('/')
+    filename = filename_t[len(filename_t)-1]
+    with open(path+'/'+filename, 'wb') as f:
+        f.write(r.content)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/80.0.3987.149 Safari/537.36 '
+}
+title = ''
+json_url = ''
+if not os.path.exists("export.json"):
+    r = requests.get(json_url, stream=True)
+    f = open("export.json", "wb")
+    for chunk in r.iter_content(chunk_size=512):
+        if chunk:
+            f.write(chunk)
+with open("export.json","r",encoding='utf-8') as data:
+    json_obj = json.load(data)
+
+
+for i in json_obj:
+    title = i['title']
+    if not os.path.exists(title):
+        os.mkdir(title)
+
+    r = requests.get(i['cover'], headers=headers)
+    with open(i['title']+"/cover.jpg", 'wb') as f:
+        f.write(r.content)
+
+    chaper = ''
+    for k in i['chapters']:
+        threads = []
+        cp_title = k['title']
+        if not os.path.exists(title+'/'+cp_title):
+            os.mkdir(title+'/'+cp_title)
+
+        print('Downloading ' + cp_title)
+        for j in k['images']:
+            t = Thread(target=download_img,args=[title+'/'+cp_title,j])
+            t.start()
+            threads.append(t)
+        for t in threads:
+            t.join()
+
